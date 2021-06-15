0bfc18ae4f50a4b0a34d32dc53a17073b1ea276c
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-06-15 15:32:15 +0200

    add config to start.sh

--- niryo/start.sh
+++ niryo/start.sh
@@ -1,6 +1,16 @@
 sudo service ssh start 
 sudo service nginx start
 
+ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST="${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST:-localhost}"
+ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT="${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT:-5555}"
+WS_EP="ws://${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST}:${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT}/ros/bridge"
+
+echo '{"defaultROSEndpoint": "${WS_EP}"}' > \
+	/home/${NB_USER}/.jupyter/lab/user-settings/jupyterlab-webviz/plugin.jupyterlab-settings;
+echo '{"defaultROSEndpoint": "${WS_EP}"}' > \
+	/home/${NB_USER}/.jupyter/lab/user-settings/jupyterlab-zethus/settings.jupyterlab-settings;
+
 source ~/catkin_ws/devel/setup.bash
+
 roscore &
 roslaunch --wait /launchfiles/fake_with_web.launch &
