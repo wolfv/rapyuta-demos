1fd022c7c14811f730ffaccb84c079ac2b50f102
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-06-18 17:09:07 +0200

    fix up start.sh some more

--- niryo/start.sh
+++ niryo/start.sh
@@ -5,10 +5,15 @@ export HOME=/home/${NB_USER}
 
 ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST="${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST:-localhost}"
 ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT="${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT:-5555}"
-WS_EP="ws://${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST}:${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT}/ros/bridge"
 
-VNC_HOST="${VNC_HOST:-localhost}"
-VNC_PORT="${VNC_PORT:-6901}"
+if [[ "$ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT" == "443" ]]; then
+	WS_EP="wss://${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST}:${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT}/ros/bridge"
+else
+	WS_EP="ws://${ROSBRIDGE_WEBSOCKET_ENDPOINT_HOST}:${ROSBRIDGE_WEBSOCKET_ENDPOINT_PORT}/ros/bridge"
+fi
+
+VNC_EXTERNAL_HOST="${VNC_EXTERNAL_HOST:-localhost}"
+VNC_EXTERNAL_PORT="${VNC_EXTERNAL_PORT:-6901}"
 
 mkdir -p ~/.jupyter/lab/user-settings/jupyterlab-webviz/
 mkdir -p ~/.jupyter/lab/user-settings/jupyterlab-zethus/
@@ -19,7 +24,7 @@ printf '{"defaultROSEndpoint": "%s"}' "${WS_EP}" > \
 
 printf '{"defaultROSEndpoint": "%s"}' "${WS_EP}" > \
 	~/.jupyter/lab/user-settings/jupyterlab-zethus/settings.jupyterlab-settings;
-printf '{"configured_endpoints": [{"name": "Default", "autoconnect": true,"reconnect": true,"reconnect_delay": 1000, "host": "%s","port": "%s", "logging": "info","resize": "scale"}]}' "${VNC_HOST}" "${VNC_PORT}" > \
+printf '{"configured_endpoints": [{"name": "Default", "autoconnect": true,"reconnect": true,"reconnect_delay": 1000, "host": "%s","port": "%s", "logging": "info","resize": "scale"}]}' "${VNC_EXTERNAL_HOST}" "${VNC_EXTERNAL_PORT}" > \
 	~/.jupyter/lab/user-settings/jupyterlab-novnc/jupyterlab-novnc-settings.jupyterlab-settings;
 
 source ~/catkin_ws/devel/setup.bash
