aeb86578a9f05b173ae07f38568601f7cfed8f5c
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-07-02 18:07:53 +0200

    rename server

--- base_docker/run-turtle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source devel/setup.bash
-envsubst < /home/js_templates/env.template.js > /home/js_templates/env.js
-sudo cp -rf /home/js_templates/env.js /usr/share/nginx/html/env.js
-
-roscore & 
-roslaunch --wait io_turtle_sim_env simulation.launch &
-roslaunch --wait io_turtle_command_center command_center.launch websocket_external_port:=`echo ${WS_ENV_PORT}` &
-roslaunch --wait io_turtle turtle.launch &
